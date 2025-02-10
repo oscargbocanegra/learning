871034f300d9f1d9243a4f85243b4ac59d68c610
oscar giovanni <oscargiovanni@gmail.com>
2025-02-10 16:54:00 -0500

    update architecture

--- 06-testing-automation/selenium-framework/src/test/java/util/driver/DriverFactory.java
+++ 06-testing-automation/selenium-framework/src/test/java/util/driver/DriverFactory.java
@@ -6,26 +6,23 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-
 public class DriverFactory {
 
     private static String pathFormat = System.getProperty("user.dir") + "\\src\\test\\resources\\drivers\\windows\\%s";
 
     public static WebDriver get(Browser browser) {
-
-        if(browser == Browser.CHROME) {
+        if (browser == Browser.CHROME) {
             System.setProperty("webdriver.chrome.driver", String.format(pathFormat, "chromedriver.exe"));
             return new ChromeDriver();
         }
-        if(browser == Browser.FIREFOX) {
-            System.setProperty("webdriver.chrome.driver", String.format(pathFormat, "geckodriver.exe"));
+        if (browser == Browser.FIREFOX) {
+            System.setProperty("webdriver.gecko.driver", String.format(pathFormat, "geckodriver.exe")); // Corregido
             return new FirefoxDriver();
         }
-        if(browser == Browser.EDGE) {
-            System.setProperty("webdriver.chrome.driver", String.format(pathFormat, "msedgedriver.exe"));
+        if (browser == Browser.EDGE) {
+            System.setProperty("webdriver.edge.driver", String.format(pathFormat, "msedgedriver.exe")); // Corregido
             return new EdgeDriver();
         }
         throw new IllegalArgumentException("Browser not supported: " + browser);
     }
-
 }
