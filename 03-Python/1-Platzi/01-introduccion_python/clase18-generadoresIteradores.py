2dadb517d6f4e8b6b55fbe0d5a79522ee34c3818
oscar giovanni <oscargiovanni@gmail.com>
2025-03-05 21:02:01 -0500

    iterator-generator

---
+++ 03-Python/1-Platzi/01-introduccion_python/clase18-generadoresIteradores.py
@@ -0,0 +1,30 @@
+# Ejemplo Iterador
+my_list = [1, 2, 3, 4, 5]
+my_iter = iter(my_list)
+
+#Usar Iterador
+print(next(my_iter))
+print(next(my_iter))
+print(next(my_iter))
+print(next(my_iter))
+print(next(my_iter))
+
+print('************************************')
+
+text = "Hola Mundo"
+my_iter = iter(text)
+print(next(my_iter)) 
+
+# Iterador con for
+for i in my_iter:
+    print(i)
+
+print('************************************')
+
+limit = 10
+
+odd_itter = iter(range(1, limit+1,2))
+for nun in odd_itter:
+    print(nun)
+
+
