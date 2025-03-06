3a8a2c1a17b401c1300d4dcf6cd11df468f0f39a
oscar giovanni <oscargiovanni@gmail.com>
2025-03-05 20:24:03 -0500

    update compresion list

--- 03-Python/1-Platzi/01-introduccion_python/clase15_comprehesionList.py
+++ 03-Python/1-Platzi/01-introduccion_python/clase15_comprehesionList.py
@@ -1,5 +1,6 @@
 squares = [x ** 2 for x in range(1,11)]
 print(squares)
+
 celsius = [0, 10, 15, 20, 25, 30, 35, 40]
 fahrenheits = [((9/5) * temp + 32) for temp in celsius]
 print("Temperatura en F ",fahrenheits)
@@ -11,7 +12,7 @@ matrix = [[1, 2, 3],
           [4, 5, 6], 
           [7, 8, 9]]
 
-transposed = [[row[i] for row in matrix] for i in range(3)]
+transposed = [[row[i] for row in matrix] for i in range(len(matrix[0]))]
 
 print(matrix)
 print(transposed)
