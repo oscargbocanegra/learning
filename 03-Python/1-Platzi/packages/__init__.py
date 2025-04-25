61666f0820fcdb09abb4eb48cd3fa0ba13529a18
oscar giovanni <oscargiovanni@gmail.com>
2025-04-24 19:26:54 -0500

    projects, grafico de poblaciones mas pobladas

--- 03-Python/1-Platzi/packages/__init__.py
+++ 03-Python/1-Platzi/packages/__init__.py
@@ -1,5 +1,5 @@
-import packages.mod_1, packages.mod_2
+import packages.mod_1, packages.mod_2, packages.charts, packages.read_csv
 
-print('Paquete inicializado')
-URL = 'https://platzi.com/'
+#print('Paquete inicializado')
+#URL = 'https://platzi.com/'
 
